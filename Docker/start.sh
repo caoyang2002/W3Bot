c290c814f4bd55fd2bcd896e076e19b1298439b2
caoyang2002 <cy12968@163.com>
2024-10-17 15:56:08 +0800

    update status bash

--- Docker/start.sh
+++ Docker/start.sh
@@ -1,49 +1,51 @@
 #!/bin/bash
 
-echo "检查是否存在 W3Bot 容器"
+set -e  # 启用错误检查，如果任何命令失败脚本将立即退出
+
+function check_command_status {
+    if [ $? -ne 0 ]; then
+        echo "错误: 上一个命令执行失败，脚本终止。"
+        exit 1
+    fi
+}
+
+echo "检查当前 Docker 容器状态"
 sudo docker ps -a
+check_command_status
 
-echo "检查是否存在 W3Bot 镜像"
+echo "检查当前 Docker 镜像状态"
 sudo docker images
+check_command_status
 
-# 检查是否有 docker-w3bot 容器
-if [ "$(sudo docker ps -aq -f name=docker-w3bot)" ]; then
-  echo "Removing existing docker-w3bot container..."
-  sudo docker rm -f docker-w3bot
-fi
+# 删除相关容器
+echo "删除相关容器..."
+docker ps -a --format '{{.ID}}\t{{.Names}}' | grep -E 'docker-w3bot|W3Bot' | awk '{print $1}' | xargs -r docker rm -f
+check_command_status
 
-# 检查是否有 W3Bot 容器
-if [ "$(sudo docker ps -aq -f name=W3Bot)" ]; then
-  echo "Removing existing W3Bot container..."
-  sudo docker rm -f W3Bot
-fi
+# 删除相关镜像
+echo "删除相关镜像..."
+docker images --format '{{.ID}}\t{{.Repository}}' | grep -E 'docker-w3bot|W3Bot' | awk '{print $1}' | xargs -r docker rmi
+check_command_status
 
-# 检查是否有 docker-w3bot 镜像
-if [ "$(sudo docker images -q docker-w3bot:latest)" ]; then
-  echo "Removing existing docker-w3bot image..."
-  sudo docker rmi docker-w3bot:latest
-fi
-
-# 检查是否有 W3Bot 镜像
-if [ "$(sudo docker images -q W3Bot:latest)" ]; then
-  echo "Removing existing W3Bot image..."
-  sudo docker rmi W3Bot:latest
-fi
-
-echo "再次检查是否存在 W3Bot 容器"
+echo "再次检查 Docker 容器状态"
 sudo docker ps -a
+check_command_status
 
-echo "再次检查是否存在 W3Bot 镜像"
+echo "再次检查 Docker 镜像状态"
 sudo docker images
+check_command_status
 
 # 构建新的镜像
 echo "构建 caoyang2002/w3bot:latest 镜像..."
 sudo docker build -t caoyang2002/w3bot:latest .
+check_command_status
 
 echo "已构建镜像: "
 sudo docker images
+check_command_status
 
 echo "启动镜像"
-sudo docker-compose up
+sudo docker-compose up -d  # 添加 -d 参数使容器在后台运行
+check_command_status
 
 echo "执行完毕"
