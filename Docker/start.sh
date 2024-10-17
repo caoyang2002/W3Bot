ef3851b5656668903c173a780caa22b227830079
caoyang2002 <cy12968@163.com>
2024-10-17 15:51:13 +0800

    update docker command

--- Docker/start.sh
+++ Docker/start.sh
@@ -1,28 +1,40 @@
 #!/bin/bash
 
+echo "检查是否存在 W3Bot 容器"
+sudo docker ps -a
+
+echo "检查是否存在 W3Bot 镜像"
+sudo docker images
+
 # 检查是否有 docker-w3bot 容器
-if [ "$(docker ps -aq -f name=docker-w3bot)" ]; then
+if [ "$(sudo docker ps -aq -f name=docker-w3bot)" ]; then
   echo "Removing existing docker-w3bot container..."
   docker rm -f docker-w3bot
 fi
 
 # 检查是否有 W3Bot 容器
-if [ "$(docker ps -aq -f name=W3Bot)" ]; then
+if [ "$(sudo docker ps -aq -f name=W3Bot)" ]; then
   echo "Removing existing W3Bot container..."
-  docker rm -f W3Bot
+  sudo docker rm -f W3Bot
 fi
 
 # 检查是否有 docker-w3bot 镜像
-if [ "$(docker images -q docker-w3bot:latest)" ]; then
+if [ "$(sudo docker images -q docker-w3bot:latest)" ]; then
   echo "Removing existing docker-w3bot image..."
-  docker rmi docker-w3bot:latest
+  sudo docker rmi docker-w3bot:latest
 fi
 
 # 检查是否有 W3Bot 镜像
-if [ "$(docker images -q W3Bot:latest)" ]; then
+if [ "$(sudo docker images -q W3Bot:latest)" ]; then
   echo "Removing existing W3Bot image..."
-  docker rmi W3Bot:latest
+  sudo docker rmi W3Bot:latest
 fi
+
+echo "再次检查是否存在 W3Bot 容器"
+sudo docker ps -a
+
+echo "再次检查是否存在 W3Bot 镜像"
+sudo docker images
 
 # 构建新的镜像
 echo "构建 caoyang2002/w3bot:latest 镜像..."
