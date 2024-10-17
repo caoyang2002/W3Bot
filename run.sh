d50140c0faf3fb4a317d1dff919ba85d6e602ea7
caoyang2002 <cy12968@163.com>
2024-10-17 13:01:29 +0800

    update shell script

--- run.sh
+++ run.sh
@@ -6,5 +6,7 @@
 #########################################################################
 #!/bin/bash
 
+# 此脚本用于在 Windows 系统上运行
+# 推荐使用 Docker/start.sh 脚本
 pip install -r requirements.txt
 python3 start.py
