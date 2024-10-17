7f5a3033c98faacf85b427dce4cff06be08478d1
caoyang2002 <cy12968@163.com>
2024-10-17 18:36:36 +0800

    pip to pip3

--- Docker/root/payloads/003-install-W3Bot-dep.sh
+++ Docker/root/payloads/003-install-W3Bot-dep.sh
@@ -9,10 +9,10 @@ cd /home/app/W3Bot || exit
 
 # 使用官方 PyPI 库安装 Python 包
 echo "Installing Python packages from official PyPI repository..."
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt -i https://pypi.org/simple
 
 # 使用 wine 和官方 PyPI 库安装 Python 包
 echo "Installing Python packages using wine from official PyPI repository..."
-wine pip3 install -r requirements.txt
+wine pip3 install -r requirements.txt -i https://pypi.org/simple
 
 echo "Package installation completed."
