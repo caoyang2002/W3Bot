ddb22f34f9ef06a7f729099915da3bc6bbd9843d
caoyang2002 <cy12968@163.com>
2024-10-17 17:06:08 +0800

    update install w3bot

--- Docker/root/payloads/003-install-W3Bot-dep.sh
+++ Docker/root/payloads/003-install-W3Bot-dep.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+# cd /home/app/W3Bot || exit
+# pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+# wine pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+# 切换到指定目录
 cd /home/app/W3Bot || exit
-pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
-wine pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+# 使用官方 PyPI 库安装 Python 包
+echo "Installing Python packages from official PyPI repository..."
+pip install -r requirements.txt
+
+# 使用 wine 和官方 PyPI 库安装 Python 包
+echo "Installing Python packages using wine from official PyPI repository..."
+wine pip install -r requirements.txt
+
+echo "Package installation completed."
